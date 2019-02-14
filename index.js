78a858ee281dd35be7e69770c94f3a986a411dc7
Adelinestack <adeline.colombo@stackerine.com>
2019-02-14 10:36:39 +0100

    commit with 2 routes

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/hello', (req, res) => {
+  res.send('Hello');
+});
+
+app.get('/bye', (req, res) => {
+  res.send('Bye bye');
+});
+
 app.listen(5000);
